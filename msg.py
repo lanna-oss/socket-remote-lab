8bd1a03c83d87d8d3088e05778d240517cc075ba
Ouychai <ae.freeman@gmail.com>
2020-06-30 20:57:11 +0700

    msg.py check reaching to Light Server.

--- msg.py
+++ msg.py
@@ -8,10 +8,23 @@ import sys
 HOST = '192.168.4.1'  # The IP address of ESP32. 
 PORT = 1234            # The port used by the server.
 
+
+def Light_connected():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.settimeout(1)
+    result = sock.connect_ex((HOST,PORT))
+    if result == 0:
+        return True
+    else:
+        return False
+
 def main():
-    
+    if not Light_connected():
+        print("Can not connect to the Light Server.")
+        sys.exit(1)
+
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    Message = input("Type any message to send to ESP32 = ")
+    Message = input("Type a command to ESP32 = ")
     
     try:
         s.connect((HOST, PORT))
