59a2df0c7016e29770782a6eb04701783eaf5507
Ouychai <ae.freeman@gmail.com>
2020-07-18 15:35:40 +0700

    Add errors checking in Light Blub Object.

--- Light_Bulb.py
+++ Light_Bulb.py
@@ -5,6 +5,7 @@ class LightBulb:
     HOST = '192.168.4.1'  # The IP address of ESP32. 
     PORT = 1234           # The port used by the Light Server.
     color = ""
+    Label = ""
     connected = False
     error_msg = ""
     def __init__(self,color):
@@ -25,33 +26,59 @@ class LightBulb:
     def status(self):
         if self.connected:
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.connect((self.HOST, self.PORT))
-            Message = self.color+" status"
-            s.send(Message.encode("ascii"))
-            msg_reply = s.recv(10)
-            s.close()
+            s.settimeout(1)
+            try:
+                s.connect((self.HOST, self.PORT))
+            except socket.error as err:
+                self.error_msg = err
+                msg_reply = "Unreachable"
+            else:
+                Message = self.color+" status"
+                s.send(Message.encode("ascii"))
+                msg_reply = s.recv(10)
+            finally:
+                s.close()
             return msg_reply.decode("utf8")
         else:
-            return "Not connect."
+            return "Not connected"
 
     def on(self):
         if self.connected:
-            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.settimeout(1)
+            try:
                 s.connect((self.HOST, self.PORT))
+            except socket.error as err:
+                self.error_msg = err
+                msg_reply = "Uneachable"
+            else:
                 Message = self.color+" on"
                 s.send(Message.encode("ascii"))
                 msg_reply = s.recv(10)
+            finally:
+                s.close
             return msg_reply.decode("utf8")
+        else:
+            return "Not connected"
 
     def off(self):
         if self.connected:
-            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.settimeout(1)
+            try:
                 s.connect((self.HOST, self.PORT))
+            except socket.error as err:
+                self.error_msg = err
+                msg_reply = "Uneachable"
+            else:                            
                 Message = self.color+" off"
                 s.send(Message.encode("ascii"))
                 msg_reply = s.recv(10)
+            finally:
+                s.close
             return msg_reply.decode("utf8")
+        else:
+            return "Not connected"
 
     def __del__(self):
         return 0
-
