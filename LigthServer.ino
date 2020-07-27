a0e386bac915be20c521218e9a40e46c8b2a0b6e
Ouychai <ae.freeman@gmail.com>
2020-07-27 20:08:41 +0700

    Check for empty message and make logs look simple.

--- LigthServer.ino
+++ LigthServer.ino
@@ -25,10 +25,9 @@ void setup() {
   WiFi.begin(WIFI_STA_NAME, WIFI_STA_PASS);
 */
   WiFi.softAP(WIFI_AP_NAME,WIFI_AP_PASS);
-
   Serial.print("ESP32 IP address: ");
   Serial.println(WiFi.softAPIP());
-
+  Serial.println("The LightServer awaits remote command.");
 
   LightServer.begin();
 }
@@ -37,67 +36,84 @@ void loop() {
 
   WiFiClient client = LightServer.available();
   if (client) {
-    char Python_msg[255] = "";
+    char Remoter_msg[255] = "";
     while (client.connected()) {
-      Serial.println("Remoter connected.");
+      Serial.print("Remoter connected.");
       while(client.available()> 0) {
         char c = client.read();
-        strncat(Python_msg, &c,1);
+        strncat(Remoter_msg, &c,1);
       }
-      Serial.print("Python Message: ");
-      Serial.println(Python_msg);
-
-      if(strcmp(Python_msg,"red on")==0){
+      Serial.print(" Text = ");
+      Serial.print(Remoter_msg);
+      if (Remoter_msg[0] == '\0'){ 
+         Serial.print("--Empty--");
+         }
+      else {
+      Serial.print(" Send = ");
+      if(strcmp(Remoter_msg,"red on")==0){
          digitalWrite(RedPin, HIGH);
          client.write("Red on");
+         Serial.print("Red on");
       } else 
-      if(strcmp(Python_msg,"red off")==0){
+      if(strcmp(Remoter_msg,"red off")==0){
          digitalWrite(RedPin, LOW);
          client.write("Red off");
+         Serial.print("Red off");
       } else
-      if(strcmp(Python_msg,"red status")==0){
-         if (digitalRead(RedPin) == 0) {client.write("OFF");} else
-         if (digitalRead(RedPin) == 1) {client.write("ON");}         
+      if(strcmp(Remoter_msg,"red status")==0){
+         if (digitalRead(RedPin) == 0) {client.write("OFF"); Serial.print("OFF");} else
+         if (digitalRead(RedPin) == 1) {client.write("ON");  Serial.print("ON"); }         
       } else
-      if(strcmp(Python_msg,"green on")==0){
+      if(strcmp(Remoter_msg,"green on")==0){
          digitalWrite(GreenPin, HIGH);
          client.write("Green on");
+         Serial.print("Green on");
       } else 
-      if(strcmp(Python_msg,"green off")==0){
+      if(strcmp(Remoter_msg,"green off")==0){
          digitalWrite(GreenPin, LOW);
          client.write("Green off");
+         Serial.print("Green off");
       } else
-      if(strcmp(Python_msg,"green status")==0){
-         if (digitalRead(GreenPin) == 0) {client.write("OFF");} else
-         if (digitalRead(GreenPin) == 1) {client.write("ON");}         
+      if(strcmp(Remoter_msg,"green status")==0){
+         if (digitalRead(GreenPin) == 0) {client.write("OFF"); Serial.print("OFF"); } else
+         if (digitalRead(GreenPin) == 1) {client.write("ON");  Serial.print("ON"); }         
       } else    
-      if(strcmp(Python_msg,"yellow on")==0){
+      if(strcmp(Remoter_msg,"yellow on")==0){
          digitalWrite(YellowPin, HIGH);
          client.write("Yellow on");
+         Serial.print("Yellow on");
        } else 
-       if(strcmp(Python_msg,"yellow off")==0){
+       if(strcmp(Remoter_msg,"yellow off")==0){
          digitalWrite(YellowPin, LOW);
          client.write("Yellow off");
+         Serial.print("Yellow off");
        } else
-       if(strcmp(Python_msg,"yellow status")==0){
-         if (digitalRead(YellowPin) == 0) {client.write("OFF");} else
-         if (digitalRead(YellowPin) == 1) {client.write("ON");}         
+       if(strcmp(Remoter_msg,"yellow status")==0){
+         if (digitalRead(YellowPin) == 0) {client.write("OFF"); Serial.print("OFF"); } else
+         if (digitalRead(YellowPin) == 1) {client.write("ON");  Serial.print("ON"); }         
        } else 
-       if(strcmp(Python_msg,"blue on")==0){
+       if(strcmp(Remoter_msg,"blue on")==0){
          digitalWrite(BluePin, HIGH);
          client.write("Blue on");
+         Serial.print("Blue on");
        } else 
-       if(strcmp(Python_msg,"blue off")==0){
+       if(strcmp(Remoter_msg,"blue off")==0){
          digitalWrite(BluePin, LOW);
-         client.write("Blue off");      
+         client.write("Blue off");
+         Serial.print("Blue off");
        } else
-       if(strcmp(Python_msg,"blue status")==0){
-         if (digitalRead(BluePin) == 0) {client.write("OFF");} else
-         if (digitalRead(BluePin) == 1) {client.write("ON");}         
-       } else { client.write("Not command"); }
+       if(strcmp(Remoter_msg,"blue status")==0){
+         if (digitalRead(BluePin) == 0) {client.write("OFF"); Serial.print("OFF");} else
+         if (digitalRead(BluePin) == 1) {client.write("ON");  Serial.print("ON"); }         
+       } else 
+         { 
+         client.write("Not command");
+         Serial.print("Not command");
+         }
+      }
           
     client.stop();
-    Serial.println("Disonnected");
+    Serial.println(" Disonnected.");
     }
   }
 }
